0442ddf8cd8d202a53801f3706bb981ef54bad38
RedheatWei <qjyyn@qq.com>
2017-07-22 20:01:37 +0800

    del

--- 实验/.metadata/.plugins/org.python.pydev.shared_interactive_console/history.py
+++
@@ -1,4 +0,0 @@
-import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
-import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
-ip = '123.103.108.90'
-print get_area(ip)
